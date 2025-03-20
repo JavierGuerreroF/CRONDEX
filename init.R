72d536e1fff6208d45dc9f53e439ea251ade04b2
JavierGuerrero <60070715+JavierGuerreroF@users.noreply.github.com>
2025-03-20 18:57:26 +0100

    heroku files

---
+++ init.R
@@ -0,0 +1,20 @@
+install.packages(c(
+  "shiny", 
+  "shinydashboard", 
+  "shinyWidgets", 
+  "DT", 
+  "tidyverse",
+  "RColorBrewer",
+  "ggrepel", 
+  "shinycssloaders",
+  "dplyr",
+  "tidyr",
+  "shinyalert",
+  "visNetwork", 
+  "waiter",
+  "plotly",
+  "upsetjs"
+))
+
+# Si usas GitHub para paquetes específicos, agrégalos así:
+# remotes::install_github("upsetjs/upsetjs_r")
