0ff638cd4f0a86646cb8787069b96bf18908b477
JavierGuerrero <60070715+JavierGuerreroF@users.noreply.github.com>
2025-03-21 00:42:58 +0100

    ragg fixing

--- init.R
+++ init.R
@@ -1,1 +1,7 @@
+# Instalar ragg con dependencias correctas
+install.packages("ragg", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+install.packages("textshaping", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+# Instalar tidyverse después de ragg
+install.packages("tidyverse", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+
 install.packages(c("UpSetR", "eulerr", "upsetjs", "plotly", "waiter", "visNetwork", "shinyalert", "shinycssloaders", "ggrepel", "RColorBrewer", "lubridate", "forcats", "stringr", "dplyr", "purrr", "readr", "tidyr", "tibble", "ggplot2", "tidyverse", "DT", "shinyWidgets", "shinydashboard", "shiny"))
